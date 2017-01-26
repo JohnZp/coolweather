5c6b904f5b981b9b03ceb1a9c9447fd8baa1262d
John <john@gmail.com>
2017-01-26 14:44:19 +0800

    增加下拉刷新和自动更新服务

--- app/src/main/java/com/weather/android/WeatherActivity.java
+++ app/src/main/java/com/weather/android/WeatherActivity.java
@@ -4,11 +4,15 @@ import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.weather.android.fragment.WeatherFragment;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    private long exitTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,4 +26,18 @@ public class WeatherActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_weather, fragment, "weather").commit();
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (System.currentTimeMillis() - exitTime > 2000) {
+                Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
